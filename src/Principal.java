3196b5cb09b55b4d08ac6da7e80b61170a1e2681
xp <xp@CETYSDAM00>
2021-11-25 13:19:30 +0100

    Empezado

---
+++ src/Principal.java
@@ -0,0 +1,181 @@
+
+import java.io.File;
+import javax.swing.JFileChooser;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xp
+ */
+public class Principal extends javax.swing.JFrame {
+    
+    SAX gesSAX = new SAX();
+
+    public Principal() {
+        initComponents();
+    }
+
+    private File seleccionarFichero(){
+        
+        File fichero = null;
+        
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        if (JFileChooser.APPROVE_OPTION == fileChooser.showOpenDialog(infoLabel)) {
+            fichero = fileChooser.getSelectedFile();
+        }
+        
+        return fichero;
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        infoLabel = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        infoLabel.setText("Abre un fichero XML para buscar");
+
+        jButton1.setText("Explorar..");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Buscar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 588, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(infoLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton2)))
+                .addContainerGap(46, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(infoLabel)
+                    .addComponent(jButton1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(31, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        File ficheroXML = seleccionarFichero();
+        gesSAX.abrir_XML_SAX(ficheroXML);
+        
+        if(ficheroXML == null){
+            this.infoLabel.setText("Vuelve a seleccionar el fichero");
+        }
+        else{
+            if(gesSAX.abrir_XML_SAX(ficheroXML) == -1){
+                this.infoLabel.setText("Error al crear el objeto SAX");
+            }else{
+                this.infoLabel.setText("Objeto SAX creado");
+            }
+
+        }
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        String mostrarAqui = gesSAX.recorrerSAX();
+        jTextArea1.append(mostrarAqui);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Principal().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel infoLabel;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/SAX.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAX {
+
+    SAXParser parser;
+    ManejadorSAX sh;
+    File ficheroXML;
+
+    public int abrir_XML_SAX(File fichero){
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            parser = factory.newSAXParser();
+
+            sh = new ManejadorSAX();
+
+            ficheroXML = fichero;
+
+            return 0;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+    
+    public String recorrerSAX(){
+        try {
+            sh.cadena_resultado = "";
+            parser.parse(ficheroXML, sh);
+            return sh.cadena_resultado;
+        } catch(SAXException ex){
+            return "Error al parsear SAX ";
+        } catch (IOException e) {
+            return "Error al parsear el SAX";
+        }
+    }
+}
+
+ class ManejadorSAX extends DefaultHandler {
+
+        String cadena_resultado = "";
+
+        @Override
+        public void characters(char[] ch, int start, int length)
+        throws SAXException{
+            for(int i = start; i<length+start; i++){
+                cadena_resultado = cadena_resultado + ch[i];
+            }
+            cadena_resultado = cadena_resultado.trim() + "\n";
+        }
+        
+        @Override
+        public void endElement(String uri, String localName, String qName)
+        throws SAXException{
+            if(qName.equals("Tienda")){
+                cadena_resultado = cadena_resultado + "-------------------------------------------\n";
+            }
+        }
+        
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes)
+        throws SAXException{
+            if(qName.equals("Tienda")){
+                cadena_resultado = cadena_resultado + "Se van a mostrar los libros de este documento. \n -------------------------------------------".trim();
+            }else if(qName.equals("Hombre")){
+                cadena_resultado = cadena_resultado + "Publicado en: " + attributes.getValue(attributes.getQName(0).trim());
+            }else if(qName.equals("Mujer")){
+                cadena_resultado = cadena_resultado + "El titulo es: ".trim();
+            }else if(qName.equals("Autor")){
+                cadena_resultado = cadena_resultado + "El autor es: ".trim();
+            }
+        }
+
+    }
